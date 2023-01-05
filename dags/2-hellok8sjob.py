cd6feaed2427517e4e0cf33bd09724a4530020c6
samuele-cozzi <samuele.cozzi@gmail.com>
2023-01-05 22:15:39 +0100

    .

--- dags/2-hellok8sjob.py
+++ dags/2-hellok8sjob.py
@@ -12,19 +12,28 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=1)
 }
 
 dag = DAG(
-    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'hello_k8s_job', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
-passing = KubernetesPodOperator(namespace='default',
-                          image="Python:3.6",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
+# passing = KubernetesPodOperator(namespace='default',
+#                           image="Python:3.6",
+#                           cmds=["Python","-c"],
+#                           arguments=["print('hello world')"],
+#                           labels={"foo": "bar"},
+#                           name="passing-test",
+#                           task_id="passing-task",
+#                           get_logs=True,
+#                           dag=dag
+#                           )
+
+passing = KubernetesPodOperator(namespace='airflow-jobs',
+                          image="docker.io/samuelecozzi/job-sample",
                           labels={"foo": "bar"},
                           name="passing-test",
                           task_id="passing-task",
@@ -32,16 +41,6 @@ passing = KubernetesPodOperator(namespace='default',
                           dag=dag
                           )
 
-failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:1604",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
+
 
 passing.set_upstream(start)
-failing.set_upstream(start)
